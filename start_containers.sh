cf2d9c6abd5b427dfdd6733a5d6ecacb5084e5f5
Victor Mendivil <tolish_@hotmail.com>
2024-07-20 17:36:08 -0400

    Added portainer as its own stack

--- start_containers.sh
+++ start_containers.sh
@@ -8,7 +8,18 @@
 #   to fix the issue, run: dos2unix setup_environment.sh
 #
 
+# Fetch all containers
+docker ps -a
+# Stop portainer
+docker stop portainer
+# Delete portainer
+docker rm portainer
+# Get latest portainer image
+docker pull portainer/portainer-ce:latest
+# Install portainer
+sudo docker compose -f dockerhomeserver-portainer-arm64-v0.yml --env-file dockerhomeserver-portainer-arm64-v0.env up -d
+
 #docker compose -f docker-compose-traefik2-all.yml up -d
-docker compose -f docker-compose-nginx-all.yml up -d
+#docker compose -f docker-compose-nginx-all.yml up -d
 #docker compose -f docker-compose-nginx-all-ubuntu.yml up -d
 #docker compose -f docker-compose-traefik2-standalone.yml up -d
